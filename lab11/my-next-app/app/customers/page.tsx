844e0c5ebff3ba23d8be041e31b7ab23d56ae59d
mateuszpuk <mateuszpuk03@gmail.com>
2026-05-28 22:48:00 +0200

    add lab11

---
+++ lab11/my-next-app/app/customers/page.tsx
@@ -0,0 +1,14 @@
+export const dynamic = "force-dynamic";
+
+import NewCustomerForm from "./_components/new-customer-form";
+import CustomersTable from "./_components/customers-table";
+
+export default function CustomersPage() {
+  return (
+    <>
+      <NewCustomerForm />
+      <p>&nbsp;</p>
+      <CustomersTable />
+    </>
+  );
+}
